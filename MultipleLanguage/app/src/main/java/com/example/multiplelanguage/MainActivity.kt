73bf6817492e271a8d263ddd28f12eb2d7c2fe7c
Yash <yashkrgola9012@gmail.com>
2024-10-18 18:13:01 +0530

    Multiple Language Support

---
+++ MultipleLanguage/app/src/main/java/com/example/multiplelanguage/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.multiplelanguage
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var show: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        show = findViewById(R.id.button)
+
+        show.setOnClickListener {
+            Toast.makeText(this, R.string.toast, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
